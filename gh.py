7a113469a6ddfbd3aeb044bcf8daadcda97dfc59
Attila Szabo <attila.szabo@prezi.com>
2018-12-05 13:01:34 +0100

    use logging instead of print

--- gh.py
+++ gh.py
@@ -1,12 +1,6 @@
 import github3
 import os
-
-try:
-    # Python 2
-    prompt = raw_input
-except NameError:
-    # Python 3
-    prompt = input
+import logging
 
 token = os.environ['GH_TOKEN']
 
@@ -14,5 +8,9 @@ g = github3.login(token=token)
 
 def check_pr(pr_url):
     _, _, _, user, repo, _, id, _ = pr_url.split('/')
-    pr = g.pull_request(user, repo, id)
+    try:
+        pr = g.pull_request(user, repo, id)
+    except Exception:
+        pr = None
+        logging.exception("g.pull_request failed")
     return pr
--- server.py
+++ server.py
@@ -2,38 +2,41 @@ import shelve
 import time
 import schedule
 import requests
+import logging
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from threading import Thread
 from gh import check_pr
 
+
 app = Flask(__name__)
+POLL_INTERVAL = 300
 
 
 class PullRequest(object):
     def __init__(self, url):
         self.url = url
-        self.status = 'open'
+        self.status = "open"
         self.created_at = time.time()
 
 
-@app.route("/add/", methods=['POST'])
+@app.route("/add/", methods=["POST"])
 def add():
-    with shelve.open('database') as database:
+    with shelve.open("database") as database:
         content = request.get_json()
-        prs = database.get('prs', dict())
-        url = content['url']
-        pr = PullRequest(url=content['url'])
+        prs = database.get("prs", dict())
+        url = content["url"]
+        pr = PullRequest(url=content["url"])
         prs[url] = pr
-        database['prs'] = prs
+        database["prs"] = prs
     return "OK"
 
 
 @app.route("/")
-def hello():
-    with shelve.open('database') as database:
-        prs = database.get('prs', {})
-        return "".join(prs.keys())
+def index():
+    with shelve.open("database", "r") as database:
+        prs = database.get("prs", {})
+        return jsonify([{key: {"status": prs[key].status}} for key in prs.keys()])
 
 
 def run_schedule():
@@ -43,21 +46,22 @@ def run_schedule():
 
 
 def check_prs():
-    with shelve.open('database', 'r') as database:
-        prs = database.get('prs')
+    logging.info('running schedule')
+    with shelve.open("database", "r") as database:
+        prs = database.get("prs")
         for key, pr in prs.items():
-            print("check %s" % pr.url)
-            pr = check_pr(pr.url)
-            if pr.mergeable:
-                pr.merge()
-                print("Merged %s" % pr.url)
+            logging.info("check %s" % pr.url)
+            gpr = check_pr(pr.url)
+            if gpr and gpr.mergeable:
+                try:
+                    gpr.merge()
+                    logging.info("Merged %s" % pr.url)
+                except Exception as e:
+                    logging.error("Failed to merge %s" % pr.url)
 
 
-start_time = time.time()
-
-
-if __name__ == '__main__':
-    schedule.every(60).seconds.do(check_prs)
+if __name__ == "__main__":
+    schedule.every(POLL_INTERVAL).seconds.do(check_prs)
     t = Thread(target=run_schedule)
     t.start()
-    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
+    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
